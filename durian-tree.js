e5a23d2f261ca82a87fbf9c35804f94e6192c656
Kayley <kayley.luftig@gmail.com>
2019-05-31 02:58:29 +0000

    construct employee tree to establish hierarchy

--- durian-tree.js
+++ durian-tree.js
@@ -1,9 +1,50 @@
 class Employee {
-  constructor (name, title, salary) {
+  constructor(name, title, salary) {
     this.name = name;
     this.title = title;
     this.salary = salary;
     this.boss = null;
     this.subordinates = [];
   }
+
+  addSubordinate(subordinate) {
+    this.subordinates.push(subordinate);
+    subordinate.boss = this;
+  }
+
+  get numberOfSubordinates() {
+    return this.subordinates.length;
+  }
+
+  get numberOfPeopleToCEO() {
+    let numberOfPeople = 0;
+    let currentEmployee = this;
+
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentEmployee.boss) {
+      currentEmployee = currentEmployee.boss;
+      numberOfPeople++;
+    }
+    return numberOfPeople;
+  }
+
+  hasSameBoss(employee) {
+    return this.boss === employee.boss;
+  }
 }
+
+
+const ada = new Employee("Ada", "CEO", 3000000.00);
+const craig    = new Employee("Craig", "VP Software", 1000000);
+const arvinder = new Employee("Arvinder", "Chief Design Officer", 1000000);
+const angela   = new Employee("Angela", "VP Retail", 1000000);
+const phil     = new Employee("Phil", "VP Marketing", 1000000);
+
+ada.addSubordinate(craig);
+ada.addSubordinate(arvinder);
+ada.addSubordinate(angela);
+ada.addSubordinate(phil);
+
+console.log("craigs boss is:", craig.boss);
+console.log("craig has", craig.numberOfSubordinates, "subordinates");
+console.log(craig.numberOfPeopleToCEO);
